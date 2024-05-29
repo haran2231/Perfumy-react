3b462d5578b3bc111223a26d242d6217daff93cb
haran2231 <103673702+haran2231@users.noreply.github.com>
2024-05-30 00:09:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Nav from "./components/Navbar";
+import Search from "./components/Search";
+import Product from "./components/Product";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <>
+            <Nav></Nav>
+            <Search></Search>
+            <Product></Product>
+            <About></About>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+// nav bar component
+function Nav() {
+    return(
+        <nav>
+        <h1>Perfumy</h1>
+        <ul>
+            <li>Home | </li>
+            <li>Products | </li>
+            <li>About | </li>
+            <li>Contact | </li>
+        </ul>
+    </nav>)
+}
+
+export default Nav
